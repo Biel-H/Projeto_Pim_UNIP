c31a643bf2dc917ffee1e80cc7eee0b3fd5b58aa
Gabriel_ <gcamaro50@gmail.com>
2023-10-27 20:20:32 -0300

    Arrumando o codigo do questionario

--- Questionario.c
+++ Questionario.c
@@ -6,9 +6,10 @@
 int nota(float grade, float lenght) {
   float result;
   result = grade / lenght;
+  printf("_______________\n");
   printf("Resultado/média: %.2f\n", result);
   printf("Nota total: %.2f\n", grade);
-  printf("Quantidade de votos: %.0f\n\n________\n\n", lenght);
+  printf("Quantidade de votos: %.0f\n\n", lenght);
   return result;
 }
 
@@ -25,14 +26,14 @@ int main() {
   system("pause");
   system("cls");
 
-  while (controller <= 7) {
+  while (controller <= 3) {
 
-    printf("100 anos da semana de arte moderna\n\n");
+    printf("\t100 anos da semana de arte moderna\n\n");
     printf("O QUE VOCÊ ACHOU DE NOSSAS EXPOSIÇÕES?\n");
-    printf("1 -> EXCELENTE\n");
-    printf("2 -> BOA\n");
-    printf("3 -> RUIM\n");
-    printf("4 -> PÉSSIMA\n\n>>> ");
+    printf("1 -> Péssimo\n");
+    printf("2 -> Ruim\n");
+    printf("3 -> Bom\n");
+    printf("4 -> Excelente\n\n>>> ");
 
     scanf("%d", &op);
     fflush(stdin);
@@ -42,50 +43,50 @@ int main() {
       notes_op = notes_op + 1;
       lenght_op = lenght_op + 1;
       // printf("%d", notes_op);
-      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      printf("\tQUE PENA QUE VOCÊ NÃO TENHA GOSTADO!\n\n");
+      printf("O QUE PODEMOS MELHORAR?\n\nDigite sua sugestão: ");
+      fgets(z, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
     case 2:
       notes_op = notes_op + 2;
       lenght_op = lenght_op + 1;
-      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
+      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
+      fgets(x, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
     case 3:
       notes_op = notes_op + 3;
       lenght_op = lenght_op + 1;
-      printf("\tQUE PENA QUE VOCÊ NÃO TENHA GOSTADO!\n\n");
-      printf("O QUE PODEMOS MELHORAR?\n\nDigite sua sugestão: ");
-      fgets(z, 40, stdin);
-      fflush(stdin);
-      printf("Obrigado pelo Feedback!!");
-      // sleep(10);
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
       system("cls");
       break;
 
     case 4:
       notes_op = notes_op + 4;
       lenght_op = lenght_op + 1;
-      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
-      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
-      fgets(x, 40, stdin);
-      fflush(stdin);
-      printf("Obrigado pelo Feedback!!");
-      // sleep(10);
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
       system("cls");
       break;
     }
 
     printf("\tJogos olímpicos de Paris 2024 \n\n");
     printf("O QUE VC ACHOU DE NOSSAS EXPOSIÇÕES? \n");
-    printf("1 -> EXCELENTE \n");
-    printf("2 -> BOA \n");
-    printf("3 -> RUIM \n");
-    printf("4 -> PÉSSIMA\n\n>>> ");
+    printf("1 -> Péssimo \n");
+    printf("2 -> Ruim\n");
+    printf("3 -> Bom\n");
+    printf("4 -> Excelente\n\n>>> ");
 
     scanf("%d", &oa);
     fflush(stdin);
@@ -94,40 +95,40 @@ int main() {
     case 1:
       notes_oa = notes_oa + 1;
       lenght_oa = lenght_oa + 1;
-      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
+      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
+      fgets(x, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
     case 2:
       notes_oa = notes_oa + 2;
       lenght_oa = lenght_oa + 1;
-      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
+      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
+      fgets(x, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
     case 3:
       notes_oa = notes_oa + 3;
       lenght_oa = lenght_oa + 1;
-      printf("\tQUE PENA QUE VOCÊ NÃO TENHA GOSTADO!\n\n");
-      printf("O QUE PODEMOS MELHORAR?\n\nDigite sua sugestão: ");
-      fgets(z, 40, stdin);
-      fflush(stdin);
-      printf("Obrigado pelo Feedback!!");
-      // sleep(10);
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
       system("cls");
       break;
 
     case 4:
       notes_oa = notes_oa + 4;
       lenght_oa = lenght_oa + 1;
-      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
-      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
-      fgets(x, 40, stdin);
-      fflush(stdin);
-      printf("Obrigado pelo Feedback!!\n");
-      // sleep(10);
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
       system("cls");
       break;
     }
@@ -151,8 +152,8 @@ int main() {
       printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
       fgets(z, 40, stdin);
       fflush(stdin);
-      printf("Obrigado pelo Feedback!!");
-      // sleep(10);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
@@ -163,8 +164,8 @@ int main() {
       printf(" O QUE PODEMOS MELHORAR?\n\nDigite sua sugestão: ");
       fgets(z, 40, stdin);
       fflush(stdin);
-      printf("Obrigado pelo Feedback!!");
-      // sleep(10);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
       system("cls");
       break;
 
@@ -172,7 +173,7 @@ int main() {
       notes_ol = notes_ol + 3;
       lenght_ol = lenght_ol + 1;
       printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      sleep(1);
       system("cls");
       break;
 
@@ -180,14 +181,67 @@ int main() {
       notes_ol = notes_ol + 4;
       lenght_ol = lenght_ol + 1;
       printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
-      // sleep(10);
+      sleep(1);
       system("cls");
       break;
     }
+    
+    printf("\tInteligência artificial\n\n");
+    printf("O QUE VC ACHOU DE NOSSAS EXPOSIÇÕES? \n");
+    printf("1 -> Péssima\n");
+    printf("2 -> Ruim\n");
+    printf("3 -> Boa\n");
+    printf("4 -> Excelente\n\n>>> ");
+
+    scanf("%d", &ou);
+    fflush(stdin);
+
+    switch (ou) {
+    case 1:
+      notes_ou = notes_ou + 1;
+      lenght_ou = lenght_ou + 1;
+      printf("\tQUE PENA QUE VOCÊ NÃO TENHA GOSTADO!\n\n");
+      printf("O QUE PODEMOS MELHORAR?\n\nDigite sua sugestão: ");
+      fgets(z, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
+      system("cls");
+      break;
+
+    case 2:
+      notes_ou = notes_ou + 2;
+      lenght_ou = lenght_ou + 1;
+      printf("\tQUE PENA QUE VC NAO TENHA GOSTADO!\n\n");
+      printf(" O QUE PODEMOS MELHORAR??\n\nDigite sua sugestão: ");
+      fgets(x, 40, stdin);
+      fflush(stdin);
+      printf("\nObrigado pelo Feedback!!");
+      sleep(1);
+      system("cls");
+      break;
+
+    case 3:
+      notes_ou = notes_ou + 3;
+      lenght_ou = lenght_ou + 1;
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
+      system("cls");
+      break;
+
+    case 4:
+      notes_ou = notes_ou + 4;
+      lenght_ou = lenght_ou + 1;
+      printf("\tOBRIGADO! FICAMOS FELIZ QUE VOCÊ TENHA GOSTADO!\n\n");
+      sleep(1);
+      system("cls");
+      break;
+    }
+    
     controller = controller + 1;
   }
-
   nota(notes_op, lenght_op);
   nota(notes_oa, lenght_oa);
   nota(notes_ol, lenght_ol);
+  nota(notes_ou, lenght_ou);
 }
